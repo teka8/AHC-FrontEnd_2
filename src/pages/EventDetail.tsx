60efd9920aba95d7f98c6f1576fa817873012f83
Emmanuel-Alem <emmaalem012@gmail.com>
2025-11-11 15:08:21 +0300

    fix: update

--- src/pages/EventDetail.tsx
+++ src/pages/EventDetail.tsx
@@ -53,9 +53,9 @@ export default function EventDetail() {
                 <ul className="text-sm text-slate-700 dark:text-slate-300 space-y-2">
                   {data.attachments.map((a, i) => (
                     <li key={i}>
-                      <a className="text-ahc-green-dark hover:underline flex items-center gap-2" href={a.path ?? '#'} target="_blank" rel="noreferrer">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-file-text"><path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"/><polyline points="14 2 14 8 20 8"/><line x1="16" x2="8" y1="13" y2="13"/><line x1="16" x2="8" y1="17" y2="17"/><line x1="10" x2="8" y1="9" y2="9"/>
-                      </svg>{a.file_name ?? 'Attachment'}
+                      <a title={a.file_name} className="text-ahc-green-dark hover:underline flex items-center gap-2" href={a.path ?? '#'} target="_blank" rel="noreferrer">
+                      <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-file-text"><path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"/><polyline points="14 2 14 8 20 8"/><line x1="16" x2="8" y1="13" y2="13"/><line x1="16" x2="8" y1="17" y2="17"/><line x1="10" x2="8" y1="9" y2="9"/>
+                      </svg >{'Attachment ' + (i + 1)}
                       </a>
                     </li>
                   ))}
