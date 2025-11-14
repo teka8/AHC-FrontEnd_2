06bd7cc8e48452386d6d8e5f2ddd47826a0266da
teka8 <tekadefar8@gmail.com>
2025-11-14 11:09:46 +0300

    'update'

--- src/components/about/Hero.tsx
+++ src/components/about/Hero.tsx
@@ -28,12 +28,10 @@ const Hero: React.FC = () => {
                 decoding="async"
                 width="1320"
                 height="880"
-                src="https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-1320x880.jpg"
+                src="/public/images/hero/about.jpg"
                 className="attachment-large size-large"
                 alt=""
                 style={{ objectPosition: "82% 51%" }}
-                srcSet="https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-1320x880.jpg 1320w, https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-760x507.jpg 760w, https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-150x100.jpg 150w, https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-768x512.jpg 768w, https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-1536x1024.jpg 1536w, https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-2048x1365.jpg 2048w"
-                sizes="(max-width: 1320px) 100vw, 1320px"
               />
             </foreignObject>
           </g>
@@ -63,9 +61,10 @@ const Hero: React.FC = () => {
           </nav>
           <h1 className="ahehc-banner__title">About Us</h1>
           <p className="is-size-large">
-            The Africa Health Collaborative is a multistakeholder partnership
-            working towards transforming Africa's Primary Health Care systems
-            through collaboration, innovation, and sustainable solutions.
+            The Africa Health Collaborative (AHC) strengthens primary healthcare
+            across Africa by empowering youth. AHC–AAU leads this work in
+            Ethiopia, partnering with local universities to equip young people,
+            especially women, to drive health sector transformation.
           </p>
         </div>
       </div>
