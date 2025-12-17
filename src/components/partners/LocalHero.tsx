717fdad8ff8cbdf3a4561ee5d635ccb2fc089ac0
Emmanuel-Alem <emmaalem012@gmail.com>
2025-12-17 16:55:27 +0300

    update

--- src/components/partners/LocalHero.tsx
+++ src/components/partners/LocalHero.tsx
@@ -28,11 +28,10 @@ const LocalHero: React.FC = () => {
                 decoding="async"
                 width="1320"
                 height="880"
-                src="https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-1320x880.jpg"
+                src="public/images/partners/photo_2025-12-17_16-00-39.jpg"
                 className="attachment-large size-large"
                 alt=""
                 style={{ objectPosition: "82% 51%" }}
-                srcSet="https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-1320x880.jpg 1320w, https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-760x507.jpg 760w, https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-150x100.jpg 150w, https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-768x512.jpg 768w, https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-1536x1024.jpg 1536w, https://africahealthcollaborative.org/wp-content/uploads/2023/12/0118TCB00919-2048x1365.jpg 2048w"
                 sizes="(max-width: 1320px) 100vw, 1320px"
               />
             </foreignObject>
